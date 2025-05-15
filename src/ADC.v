d64b91cd53cf2c6aed9012278e666df7cba88f74
Noah <braasch.noah@gmail.com>
2025-05-15 17:38:57 -0400

    Creates Basic source files

---
+++ src/ADC.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/15/2025 05:38:41 PM
+// Design Name: 
+// Module Name: ADC
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ADC(
+
+    );
+endmodule
